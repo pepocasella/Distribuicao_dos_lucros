896ca652f22e039832d5167819c2b0bbd6a57302
pepocasella <pedro_casella@gmail.com>
2019-01-26 23:48:13 -0200

    separação do código de classe do funcionario e do main

---
+++ classe_funcionarios.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 26 19:09:10 2019
+
+@author: Pedro Casella
+"""
+
+class Funcionarios:
+    
+    objetos = []
+    
+    def __init__(self,matricula,nome,area,cargo,salario_bruto,data_de_admissao):
+        self.matricula = matricula
+        self.nome = nome
+        self.area = area
+        self.cargo = cargo
+        self.salario_bruto = salario_bruto
+        self.data_de_admissao = data_de_admissao
+    
+    def calcula_parametros(self,data_de_admissao):
+        return 
+    
+
+    def calcula_bonus(self):
+        bonus = (((salario_bruto*peso_tempo_adimissao)+(peso_salario_bruto*peso_area))/(salario_bruto*peso_faixa_salarial))*12
+        return
+    
+    def save(self):
+        self.__class__.seq += 1
+        self.id = self.__class__.seq
+        self.__class__.objects.append(self)
+    
