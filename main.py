d53698814ca19dfa3f309be3b2c1ecfb51e9042c
pepocasella <pedro_casella@gmail.com>
2019-01-26 23:46:58 -0200

    separação do codigo main do codigo classes funcionarios; Alem disso criação das funcões -leitura doexcel banco de dados, utilização do docionario em python para traduzir e separar os pesos da area de atuação, condicionar os pesos por  faixa salarial com a exceção para estagiarios

--- main.py
+++ main.py
@@ -38,35 +38,58 @@ Peso 5: Mais de 8 anos
 Equação:
     bonus = (((salario_bruto * peso_tempo_adimissao) + (peso_salario_bruto * peso_area))/(salario_bruto * peso_faixa_salarial))*12
 
-
 Retorno:
     
-    
 """
+from classe_funcionarios import Funcionarios 
+from xlrd import open_workbook
 
-#Variaveis
-salario_minimo =  1006
-
-
-class Funcionario:
-    
-    def __init__(self,matricula,nome,area,cargo,salario_bruto,data_de_admissao):
-        self.matricula = matricula
-        self.nome = nome
-        self.area = area
-        self.cargo = cargo
-        self.salario_bruto = salario_bruto
-        self.data_de_admissao = data_de_admissao
-        
-    def calcula_parametros(self,data_de_admissao):
-        
-        return 
-    
-    
-    
-    def calcula_bonus(self):
-        bonus = (((salario_bruto*peso_tempo_adimissao)+(peso_salario_bruto*peso_area))/(salario_bruto*peso_faixa_salarial))*12
-        return bonus_pessoa
-        
-        
 if __name__== "__main__":
+    
+    #---------------leitura do banco de dados excel----------------------------
+    
+    wb = open_workbook('banco_dados_funcionarios.xlsx')
+    sheet = wb.sheet_by_index(0)
+        
+    for row in range(1,sheet.nrows):
+        for columns in range(0,sheet.ncols):
+            print(sheet.cell_value(row,columns))
+    print(sheet.row_values(1))
+    
+    #------------instanciando meus objetos = funcionarios----------------------
+    
+    funcionario_1 = Funcionarios(sheet.row_values(1)[0],sheet.row_values(1)[1],
+                                 sheet.row_values(1)[2],sheet.row_values(1)[3],
+                                 sheet.row_values(1)[4],sheet.row_values(1)[5])
+    
+    funcionario_2 = Funcionarios(sheet.row_values(2)[0],sheet.row_values(2)[1],
+                                 sheet.row_values(2)[2],sheet.row_values(2)[3],
+                                 sheet.row_values(2)[4],sheet.row_values(2)[5])
+    
+    funcionario_3 = Funcionarios(sheet.row_values(3)[0],sheet.row_values(3)[1],
+                                 sheet.row_values(3)[2],sheet.row_values(3)[3],
+                                 sheet.row_values(3)[4],sheet.row_values(3)[5])
+    
+    #----------------------levantamento dos pesos da equação-------------------
+    
+    #pesos por area
+    tabela_pesos_area = {"Diretoria":1,
+                    "Contabilidade":2,
+                    "Financeiro":2,
+                    "Tecnologia":2,
+                    "Serviços Gerais":3,
+                    "Relacionamento com o Cliente":5}
+    
+    #pesos por salario
+    salario_minimo = 1006
+    
+    if funcionario.salario_bruto > 8*salario_minimo:
+        peso_salario_bruto = 5
+    elif funcionario.salario_bruto < 8*salario_minimo and funcionario.salario_bruto > 5*salario_minimo:
+        peso_salario_bruto = 3
+    elif funcionario.salario_bruto < 5*salario_minimo and funcionario.salario_bruto > 3*salario_minimo:
+        peso_salario_bruto = 2
+    else:
+        peso_salario_bruto = 1
+    
+    #pesos por tempo
